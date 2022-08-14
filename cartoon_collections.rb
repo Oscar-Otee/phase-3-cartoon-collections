0f6d32cb4268ee4d75f5164460efbb78d759ab42
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-08-14 12:15:56 +0300

    commit tree d53bcb76d050a04f3f785fa45b82a7b6fa035ce5

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.map.with_index {|name, index| puts "#{index + 1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.map {|word| "#{word.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.any? {|calls_long| calls_long.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |element|
+    if element == cheese_types[0] || element == cheese_types[1] || element == cheese_types[2]
+      "#{element}"
+    else
+      nil
+    end
+  end
 end
